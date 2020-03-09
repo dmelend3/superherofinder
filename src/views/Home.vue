eb319613013d278d90b9e47ee336e7e29e9205cb
dmelend3 <santos_9602@hotmail.com>
2020-03-08 19:17:39 -0500

    Home FInished

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -28,7 +28,9 @@
         <Card
         class="sm:w-1/2 lg:w-1/3 xl:w-1/4 p-2"
         v-for="hero in heroes"
-        :key="hero.id" />  
+        :key="hero.id"
+        :hero="hero"
+        />  
       </div>
     </div>
   </div>
